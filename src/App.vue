ca3872bd746afa12681f23bc7b311d2d12d25738
rizkyRmdhni <rizkyramadhaniputra37@student.uir.ac.id>
2025-05-03 09:31:15 +0700

    addItem

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from 'vue'
+
+const items = ref([])
+const newItem = ref('')
+
+const addItem = () => {
+  if(newItem.value !== ''){
+    items.value.push({
+      id: items.value.length + 1,
+      text: newItem.value,
+      completed: false
+    })
+    newItem.value = ''
+    console.log(items.value)
+  }
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <input type="text" v-model="newItem" @keyup.enter="addItem">
+  <button @click="addItem">Tambahkan</button>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
