6e1f88014540b961e5474d2366344060495cc994
rizkyRmdhni <rizkyramadhaniputra37@student.uir.ac.id>
2025-05-03 09:50:34 +0700

    filtered item

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 
 const items = ref([])
 const newItem = ref('')
+const filter = ref('all')
 
 const addItem = () => {
   if (newItem.value !== '') {
@@ -21,17 +22,31 @@ const toggle = (item) => {
 
 const remove = (item) => {
   items.value = items.value.filter(i => i.id !== item.id)
-  console.log(items.value)
 }
+
+const filteredItem = computed(() =>{
+  if(filter.value === "completed"){
+    return items.value.filter (item => item.completed)
+  }else if(filter.value === "active"){
+    return items.value.filter(item => !item.completed)
+  }else{
+    return items.value
+  }
+})
 
 </script>
 
 <template>
   <input type="text" v-model="newItem" @keyup.enter="addItem">
   <button @click="addItem">Tambahkan</button>
+  <select v-model="filter">
+    <option value="all">Semua</option>
+    <option value="active">Aktif</option>
+    <option value="completed">Selesai</option>
+  </select>
 
   <ul>
-    <li v-for="item in items" :key="item.id">
+    <li v-for="item in filteredItem" :key="item.id">
       <input type="checkbox" :checked="item.completed" @change="toggle(item)">
       {{ item.text }}
       <button @click="remove(item)">Hapus</button>
